968a3be2e62465e3ad77a11604cd490f029dbc09
martinBosch <martinbosch17@gmail.com>
2017-03-22 16:28:31 -0300

    Cree el TDA buffer para usar en lugar de la lista

---
+++ buffer.c
@@ -0,0 +1,67 @@
+#include "buffer.h"
+#include <string.h>
+#include <stdio.h>
+
+#define TAM_INICIAL 50
+
+
+int buffer_crear(buffer_t *self) {
+    self->buffer = malloc(TAM_INICIAL * sizeof(char*));
+    if(self->buffer == NULL) {
+        return 1;
+    }
+    self->tam = TAM_INICIAL;
+    self->cant_words = 0;
+    return 0;
+}
+
+void buffer_destruir(buffer_t *self) {
+    for (int i = 0; i < self->cant_words; i+=1) {
+        free(self->buffer[i]);
+    }
+    free(self->buffer);
+}
+
+static int buffer_redimensionar(buffer_t *self, size_t tam_nuevo) {
+    char **buffer_nuevo = realloc(self->buffer, tam_nuevo * sizeof(char*));
+    // Cuando tam_nuevo es 0, es correcto que devuelva NULL
+    // En toda otra situación significa que falló el realloc
+    if (tam_nuevo > 0 && buffer_nuevo == NULL) {
+        return 1;
+    }
+    self->buffer = buffer_nuevo;
+    self->tam = tam_nuevo;
+    return 0;
+}
+
+int buffer_guardar(buffer_t *self, char *word) {
+    if (self->cant_words >= self->tam) {
+        int redimension = buffer_redimensionar(self, 2*self->tam);
+        if (redimension == 1) {
+            return 1;
+        }
+    }
+    size_t tam_word = strlen(word);
+
+//    printf("%zu\n", tam_word);
+//    printf("%s\n", word);
+
+    char *aux = malloc((tam_word+1) * sizeof(char));
+    memcpy(aux, word, tam_word+1);
+
+//    printf("AUX: %s\n", aux);
+
+    self->buffer[self->cant_words] = aux;
+    self->cant_words += 1;
+    return 0;
+}
+
+//size_t buffer_obtener_cant(buffer_t *self) {
+//    return self->cant_words;
+//}
+
+void buffer_imprimir_words(buffer_t *self) {
+    for (int i = 0; i < self->cant_words; i+=1) {
+        printf("%s\n", self->buffer[i]);
+    }
+}
---
+++ buffer.h
@@ -0,0 +1,22 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#include <stdlib.h>
+
+typedef struct {
+    char **buffer;
+    size_t tam;
+    size_t cant_words;
+} buffer_t;
+
+int buffer_crear(buffer_t *self);
+
+void buffer_destruir(buffer_t *self);
+
+int buffer_guardar(buffer_t *self, char *word);
+
+//size_t buffer_obtener_cant(buffer_t *self);
+
+void buffer_imprimir_words(buffer_t *self);
+
+#endif //BUFFER_H
