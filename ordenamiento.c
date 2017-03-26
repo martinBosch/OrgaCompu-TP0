13c5080d9f8dcc3413668349ef37e5ed3f7a724a
Luis Tejerina <lteje11@gmail.com>
2017-03-26 13:53:49 -0300

    Bug

--- ordenamiento.c
+++ ordenamiento.c
@@ -35,7 +35,7 @@ static int partition(char **vec, size_t izq, size_t der) {
     return izq;
 }
 
-void quicksort(char **vec, size_t izq, size_t der) {
+void quicksort(char **vec, int izq, int der) {
     // Si la lista esta vacia, o es de tamanio 1 esta ordenada.
     if (der-izq <= 0) {
         return;
